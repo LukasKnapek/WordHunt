07e710557bed56605ead97bec0edc03827a3e850
Lukas Knapek <knapek.luk@googlemail.com>
2018-02-28 16:50:51 +0000

    Migrated models, registered models for Django admin site

--- WordHuntApp/admin.py
+++ WordHuntApp/admin.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from WordHuntApp.models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Image)
+admin.site.register(Rating)
+admin.site.register(Comment)
+admin.site.register(Word)
+admin.site.register(Competition)
