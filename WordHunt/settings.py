a5dce92faa5cbbd5e70ea16ba724edef677561df
Justyna <2270645t@student.gla.ac.uk>
2018-03-12 15:34:05 +0000

    Layout of all templates done + some changes to settings

--- WordHunt/settings.py
+++ WordHunt/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'WordHunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
